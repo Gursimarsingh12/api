659550c4fc64d191fe88d88a136ac40b2b89c876
Gursimar Singh <141723338+Gursimarsingh12@users.noreply.github.com>
2024-08-02 17:46:56 +0530

    commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import html
+import requests
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+url = "https://sites.google.com/view/ggsipuedc/notice-board?authuser=0"
+webpage = requests.get(url=url)
+soup = BeautifulSoup(webpage.content, 'lxml')
+
+def getNotices(soup):
+    divs = soup.find_all('div', {'class': 'w536ob'})
+    data = []
+
+    for div in divs:
+        data_code_content = div.get('data-code')
+        decoded_content = html.unescape(data_code_content)
+        soup_decoded = BeautifulSoup(decoded_content, 'lxml')
+        rows = soup_decoded.find_all('tr')
+        for row in rows[1:]:
+            columns = row.find_all('td')
+            if len(columns) >= 2:
+                date = columns[0].text.strip()
+                link = columns[1].find('a')
+                if link is not None:
+                    title = link.text.strip()
+                    href = link.get('href')
+                    data.append({'date': date, 'title': title, 'link': href})
+    return data
+
+@app.get("/get")
+async def get_Notices():
+    return getNotices(soup)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
